55702dfda45bf7ffc3bee23bdfb4bf6a11f277e4
Michael Silva <michael.steam@outlook.com>
2022-02-23 09:53:23 -0300

    Adicionar arquivos de projeto.

---
+++ ProjAgendaLista/ProjAgendaLista/Agenda.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjAgendaLista
+{
+    internal class Agenda
+    {
+        //public Contato Anterior { get; set; }
+        //public Contato Registro { get; set; }  
+        //public Contato Proximo { get; set; }
+        public Contato Cabeca { get; set; }
+        public Contato Cauda { get; set; }
+
+        public Agenda()
+        {
+            //Anterior = null;
+           //Registro = null;
+           //Proximo = null;
+            Cabeca = null;
+            Cauda = null;
+
+        }
+
+        public void push(Contato novoContato)
+        {
+            if (Vazio())
+            {
+                Cabeca = novoContato;
+                //Registro = novoContato;
+                Cauda = novoContato;
+            }
+            else
+            {
+                Cauda.Proximo = novoContato;
+                Cauda = novoContato;
+                OrdenarAgenda();
+            }             
+        }
+
+        public void PrintAgenda()
+        {
+            Contato aux = Cabeca;
+            do
+            {
+                Console.WriteLine(aux.ToString());
+                aux = aux.Proximo;
+            } while (aux != null);
+        }
+
+        public bool Vazio()
+        {
+            if(Cabeca == null && Cauda == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void OrdenarAgenda()
+        {
+            /*
+             Contato referencia = Cabeca;
+             Contato comparacao = Cabeca.Proximo;
+             do
+             {
+                 do
+                 {
+                     if (String.Compare(referencia.Nome, comparacao.Nome) >= 0)
+                     {
+                         comparacao = comparacao.Proximo;
+                         referencia = referencia.Proximo;
+
+                         Contato suporte = referencia;
+                         referencia = comparacao;
+                         comparacao = suporte.Anterior;
+
+                     }
+                     comparacao = comparacao.Proximo;
+                 }while (comparacao != null);
+                 referencia = referencia.Proximo;    
+             } while (referencia != null);
+            */
+
+            for (Contato referencia = Cabeca; referencia != null; referencia = referencia.Proximo)
+            {
+                for (Contato comparacao = referencia.Proximo; comparacao != null; comparacao = comparacao.Proximo)
+                {
+                    if (referencia.Nome.CompareTo(comparacao.Nome) > 0)
+                    {
+                        Contato suporte = new Contato(referencia.Nome, referencia.Email, referencia.Telefones);
+                        suporte.Nome = referencia.Nome;
+                        suporte.Email = referencia.Email;
+                        suporte.Telefones = referencia.Telefones;
+                        referencia.Nome = comparacao.Nome;
+                        referencia.Email = comparacao.Email;
+                        referencia.Telefones = comparacao.Telefones;
+                        comparacao.Nome = suporte.Nome;
+                        comparacao.Email = suporte.Email;
+                        comparacao.Telefones = suporte.Telefones;
+                    }
+                }
+            }
+
+        }
+
+
+
+
+        /*
+        public override string ToString()
+        {
+            return Cauda.ToString();
+        }
+        */
+
+
+
+    }
+}
---
+++ ProjAgendaLista/ProjAgendaLista/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ProjAgendaLista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 0;
+
+            Agenda agenda = new Agenda();
+            //Contato c1;
+            //Telefone t1;
+
+            do
+            {
+                Console.WriteLine("Informe o nome: ");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine("Informe o email: ");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("Informe o tipo: ");
+                string tipo = Console.ReadLine();
+
+                Console.WriteLine("Informe o ddd: ");
+                int ddd = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Informe o numero: ");
+                int numero = int.Parse(Console.ReadLine());
+
+
+                agenda.push(new Contato(nome, email, new Telefone(tipo, ddd, numero)));
+                //Console.WriteLine(agenda.ToString());
+                op = int.Parse(Console.ReadLine());
+            } while (op != 123);
+            //t1 = new Telefone(tipo, ddd, numero);
+            //c1 = new Contato(nome, email, t1);
+
+            agenda.PrintAgenda();
+        }         
+    }
+}
